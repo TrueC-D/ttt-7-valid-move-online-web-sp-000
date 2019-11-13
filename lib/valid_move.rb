af1a96a543de3535c56d3dc9e917f740604b8935
Alethea Cary <aletheacary@gmail.com>
2019-11-13 13:49:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  position_taken?(board, index)
+  if board[index] == NIL
+    FALSE
+  else
+    !position_taken?(board, index)
+  end
 end
 
 
